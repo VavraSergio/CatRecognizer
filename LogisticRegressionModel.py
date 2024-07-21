e24c161d97a30918cc973f6189e296b9cd5ffadc
Sergio Vavra <sergio.vavra@gmail.com>
2024-07-21 18:01:12 +0200

    Added the utils file

--- LogisticRegressionModel.py
+++ LogisticRegressionModel.py
@@ -6,11 +6,6 @@ import scipy
 from PIL import Image
 from scipy import ndimage
 from lr_utils import load_dataset
-from public_tests import *
-
-get_ipython().run_line_magic('matplotlib', 'inline')
-get_ipython().run_line_magic('load_ext', 'autoreload')
-get_ipython().run_line_magic('autoreload', '2')
 
 # Loading the data (cat/non-cat)
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
@@ -66,44 +61,15 @@ def sigmoid(z):
     Return:
     s -- sigmoid(z)
     """
-
-    #(≈ 1 line of code)
-    # s = ...
-    # YOUR CODE STARTS HERE
     s = 1 / (1 + np.exp(-z))
-    
-    # YOUR CODE ENDS HERE
-    
+        
     return s
 
-
-# In[9]:
-
-
 print ("sigmoid([0, 2]) = " + str(sigmoid(np.array([0,2]))))
-
-sigmoid_test(sigmoid)
-
-
-# In[10]:
-
 
 x = np.array([0.5, 0, 2.0])
 output = sigmoid(x)
 print(output)
-
-
-# <a name='4-2'></a>
-# ### 4.2 - Initializing parameters
-# 
-# <a name='ex-4'></a>
-# ### Exercise 4 - initialize_with_zeros
-# Implement parameter initialization in the cell below. You have to initialize w as a vector of zeros. If you don't know what numpy function to use, look up np.zeros() in the Numpy library's documentation.
-
-# In[11]:
-
-
-# GRADED FUNCTION: initialize_with_zeros
 
 def initialize_with_zeros(dim):
     """
@@ -116,20 +82,11 @@ def initialize_with_zeros(dim):
     w -- initialized vector of shape (dim, 1)
     b -- initialized scalar (corresponds to the bias) of type float
     """
-    
-    # (≈ 2 lines of code)
-    # w = ...
-    # b = ...
-    # YOUR CODE STARTS HERE
+
     w = np.zeros((dim, 1))
     b = 0.0
     
-    # YOUR CODE ENDS HERE
-
     return w, b
-
-
-# In[12]:
 
 
 dim = 2
@@ -138,36 +95,6 @@ w, b = initialize_with_zeros(dim)
 assert type(b) == float
 print ("w = " + str(w))
 print ("b = " + str(b))
-
-initialize_with_zeros_test_1(initialize_with_zeros)
-initialize_with_zeros_test_2(initialize_with_zeros)
-
-
-# <a name='4-3'></a>
-# ### 4.3 - Forward and Backward propagation
-# 
-# Now that your parameters are initialized, you can do the "forward" and "backward" propagation steps for learning the parameters.
-# 
-# <a name='ex-5'></a>
-# ### Exercise 5 - propagate
-# Implement a function `propagate()` that computes the cost function and its gradient.
-# 
-# **Hints**:
-# 
-# Forward Propagation:
-# - You get X
-# - You compute $A = \sigma(w^T X + b) = (a^{(1)}, a^{(2)}, ..., a^{(m-1)}, a^{(m)})$
-# - You calculate the cost function: $J = -\frac{1}{m}\sum_{i=1}^{m}(y^{(i)}\log(a^{(i)})+(1-y^{(i)})\log(1-a^{(i)}))$
-# 
-# Here are the two formulas you will be using: 
-# 
-# $$ \frac{\partial J}{\partial w} = \frac{1}{m}X(A-Y)^T\tag{7}$$
-# $$ \frac{\partial J}{\partial b} = \frac{1}{m} \sum_{i=1}^m (a^{(i)}-y^{(i)})\tag{8}$$
-
-# In[13]:
-
-
-# GRADED FUNCTION: propagate
 
 def propagate(w, b, X, Y):
     """
@@ -184,37 +111,18 @@ def propagate(w, b, X, Y):
             (dw -- gradient of the loss with respect to w, thus same shape as w)
             (db -- gradient of the loss with respect to b, thus same shape as b)
     cost -- negative log-likelihood cost for logistic regression
-    
-    Tips:
-    - Write your code step by step for the propagation. np.log(), np.dot()
     """
     
     m = X.shape[1]
-    
-    # FORWARD PROPAGATION (FROM X TO COST)
-    #(≈ 2 lines of code)
-    # compute activation
-    # A = ...
-    # compute cost by using np.dot to perform multiplication. 
-    # And don't use loops for the sum.
-    # cost = ...                                
-    # YOUR CODE STARTS HERE
+
     A = 1 / (1 + np.exp(-(np.dot(w.T, X) + b)))
     cost = (-1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A))
     
-    # YOUR CODE ENDS HERE
-
-    # BACKWARD PROPAGATION (TO FIND GRAD)
-    #(≈ 2 lines of code)
-    # dw = ...
-    # db = ...
-    # YOUR CODE STARTS HERE
     
     dw = (1 / m) * np.dot(X, (A - Y).T)
     db = (1 / m) * np.sum(A - Y)
     
     
-    # YOUR CODE ENDS HERE
     cost = np.squeeze(np.array(cost))
 
     
@@ -270,26 +178,17 @@ def optimize(w, b, X, Y, num_iterations=100, learning_rate=0.009, print_cost=Fal
     costs = []
     
     for i in range(num_iterations):
-        # (≈ 1 lines of code)
-        # Cost and gradient calculation 
-        # grads, cost = ...
-        # YOUR CODE STARTS HERE
         grads, cost = propagate(w, b, X, Y)
         
-        # YOUR CODE ENDS HERE
         
         # Retrieve derivatives from grads
         dw = grads["dw"]
         db = grads["db"]
         
-        # update rule (≈ 2 lines of code)
-        # w = ...
-        # b = ...
-        # YOUR CODE STARTS HERE
+        # update rule 
         w = w - learning_rate * dw
         b = b - learning_rate * db
         
-        # YOUR CODE ENDS HERE
         
         # Record the costs
         if i % 100 == 0:
@@ -308,9 +207,6 @@ def optimize(w, b, X, Y, num_iterations=100, learning_rate=0.009, print_cost=Fal
     return params, grads, costs
 
 
-# In[16]:
-
-
 params, grads, costs = optimize(w, b, X, Y, num_iterations=100, learning_rate=0.009, print_cost=False)
 
 print ("w = " + str(params["w"]))
@@ -318,22 +214,6 @@ print ("b = " + str(params["b"]))
 print ("dw = " + str(grads["dw"]))
 print ("db = " + str(grads["db"]))
 print("Costs = " + str(costs))
-
-optimize_test(optimize)
-
-
-# <a name='ex-7'></a>
-# ### Exercise 7 - predict
-# The previous function will output the learned w and b. We are able to use w and b to predict the labels for a dataset X. Implement the `predict()` function. There are two steps to computing predictions:
-# 
-# 1. Calculate $\hat{Y} = A = \sigma(w^T X + b)$
-# 
-# 2. Convert the entries of a into 0 (if activation <= 0.5) or 1 (if activation > 0.5), stores the predictions in a vector `Y_prediction`. If you wish, you can use an `if`/`else` statement in a `for` loop (though there is also a way to vectorize this). 
-
-# In[17]:
-
-
-# GRADED FUNCTION: predict
 
 def predict(w, b, X):
     '''
@@ -353,23 +233,13 @@ def predict(w, b, X):
     w = w.reshape(X.shape[0], 1)
     
     # Compute vector "A" predicting the probabilities of a cat being present in the picture
-    #(≈ 1 line of code)
-    # A = ...
-    # YOUR CODE STARTS HERE
     
     A = 1 / (1 + np.exp(-(np.dot(w.T, X) + b)))
     
-    # YOUR CODE ENDS HERE
     
     for i in range(A.shape[1]):
         
         # Convert probabilities A[0,i] to actual predictions p[0,i]
-        #(≈ 4 lines of code)
-        # if A[0, i] > ____ :
-        #     Y_prediction[0,i] = 
-        # else:
-        #     Y_prediction[0,i] = 
-        # YOUR CODE STARTS HERE
         
         if A[0, i] > .5:
             Y_prediction[0,i] = 1
@@ -377,49 +247,16 @@ def predict(w, b, X):
             Y_prediction[0,i] = 0
         
         
-        # YOUR CODE ENDS HERE
     
     return Y_prediction
 
 
-# In[18]:
 
 
 w = np.array([[0.1124579], [0.23106775]])
 b = -0.3
 X = np.array([[1., -1.1, -3.2],[1.2, 2., 0.1]])
 print ("predictions = " + str(predict(w, b, X)))
-
-predict_test(predict)
-
-
-# <font color='blue'>
-#     
-# **What to remember:**
-#     
-# You've implemented several functions that:
-# - Initialize (w,b)
-# - Optimize the loss iteratively to learn parameters (w,b):
-#     - Computing the cost and its gradient 
-#     - Updating the parameters using gradient descent
-# - Use the learned (w,b) to predict the labels for a given set of examples
-
-# <a name='5'></a>
-# ## 5 - Merge all functions into a model ##
-# 
-# You will now see how the overall model is structured by putting together all the building blocks (functions implemented in the previous parts) together, in the right order.
-# 
-# <a name='ex-8'></a>
-# ### Exercise 8 - model
-# Implement the model function. Use the following notation:
-#     - Y_prediction_test for your predictions on the test set
-#     - Y_prediction_train for your predictions on the train set
-#     - parameters, grads, costs for the outputs of optimize()
-
-# In[19]:
-
-
-# GRADED FUNCTION: model
 
 def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
     """
@@ -437,24 +274,11 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0
     Returns:
     d -- dictionary containing information about the model.
     """
-    # (≈ 1 line of code)   
     # initialize parameters with zeros
-    # and use the "shape" function to get the first dimension of X_train
-    # w, b = ...
-    
-    #(≈ 1 line of code)
-    # Gradient descent 
-    # params, grads, costs = ...
     
     # Retrieve parameters w and b from dictionary "params"
-    # w = ...
-    # b = ...
     
     # Predict test/train set examples (≈ 2 lines of code)
-    # Y_prediction_test = ...
-    # Y_prediction_train = ...
-    
-    # YOUR CODE STARTS HERE
     w, b = initialize_with_zeros(X_train.shape[0])
     params, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
     w = params['w']
@@ -463,7 +287,6 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0
     Y_prediction_train = predict(w, b, X_train)
 
     
-    # YOUR CODE ENDS HERE
 
     # Print train/test Errors
     if print_cost:
@@ -482,27 +305,14 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0
     return d
 
 
-# In[20]:
 
 
 from public_tests import *
 
 model_test(model)
 
-
-# If you pass all the tests, run the following cell to train your model.
-
-# In[21]:
-
-
 logistic_regression_model = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=2000, learning_rate=0.005, print_cost=True)
 
-
-# **Comment**: Training accuracy is close to 100%. This is a good sanity check: your model is working and has high enough capacity to fit the training data. Test accuracy is 70%. It is actually not bad for this simple model, given the small dataset we used and that logistic regression is a linear classifier. But no worries, you'll build an even better classifier next week!
-# 
-# Also, you see that the model is clearly overfitting the training data. Later in this specialization you will learn how to reduce overfitting, for example by using regularization. Using the code below (and changing the `index` variable) you can look at predictions on pictures of the test set.
-
-# In[25]:
 
 
 # Example of a picture that was wrongly classified.
@@ -524,25 +334,6 @@ plt.xlabel('iterations (per hundreds)')
 plt.title("Learning rate =" + str(logistic_regression_model["learning_rate"]))
 plt.show()
 
-
-# **Interpretation**:
-# You can see the cost decreasing. It shows that the parameters are being learned. However, you see that you could train the model even more on the training set. Try to increase the number of iterations in the cell above and rerun the cells. You might see that the training set accuracy goes up, but the test set accuracy goes down. This is called overfitting. 
-
-# <a name='6'></a>
-# ## 6 - Further analysis (optional/ungraded exercise) ##
-# 
-# Congratulations on building your first image classification model. Let's analyze it further, and examine possible choices for the learning rate $\alpha$. 
-
-# #### Choice of learning rate ####
-# 
-# **Reminder**:
-# In order for Gradient Descent to work you must choose the learning rate wisely. The learning rate $\alpha$  determines how rapidly we update the parameters. If the learning rate is too large we may "overshoot" the optimal value. Similarly, if it is too small we will need too many iterations to converge to the best values. That's why it is crucial to use a well-tuned learning rate.
-# 
-# Let's compare the learning curve of our model with several choices of learning rates. Run the cell below. This should take about 1 minute. Feel free also to try different values than the three we have initialized the `learning_rates` variable to contain, and see what happens. 
-
-# In[24]:
-
-
 learning_rates = [0.01, 0.001, 0.0001]
 models = {}
 
@@ -562,28 +353,6 @@ frame = legend.get_frame()
 frame.set_facecolor('0.90')
 plt.show()
 
-
-# **Interpretation**: 
-# - Different learning rates give different costs and thus different predictions results.
-# - If the learning rate is too large (0.01), the cost may oscillate up and down. It may even diverge (though in this example, using 0.01 still eventually ends up at a good value for the cost). 
-# - A lower cost doesn't mean a better model. You have to check if there is possibly overfitting. It happens when the training accuracy is a lot higher than the test accuracy.
-# - In deep learning, we usually recommend that you: 
-#     - Choose the learning rate that better minimizes the cost function.
-#     - If your model overfits, use other techniques to reduce overfitting. (We'll talk about this in later videos.) 
-# 
-
-# <a name='7'></a>
-# ## 7 - Test with your own image (optional/ungraded exercise) ##
-# 
-# Congratulations on finishing this assignment. You can use your own image and see the output of your model. To do that:
-#     1. Click on "File" in the upper bar of this notebook, then click "Open" to go on your Coursera Hub.
-#     2. Add your image to this Jupyter Notebook's directory, in the "images" folder
-#     3. Change your image's name in the following code
-#     4. Run the code and check if the algorithm is right (1 = cat, 0 = non-cat)!
-
-# In[30]:
-
-
 # change this to the name of your image file
 my_image = "IMG_0857.png"   
 
@@ -597,19 +366,3 @@ my_predicted_image = predict(logistic_regression_model["w"], logistic_regression
 
 print("y = " + str(np.squeeze(my_predicted_image)) + ", your algorithm predicts a \"" + classes[int(np.squeeze(my_predicted_image)),].decode("utf-8") +  "\" picture.")
 
-
-# <font color='blue'>
-#     
-# **What to remember from this assignment:**
-# 1. Preprocessing the dataset is important.
-# 2. You implemented each function separately: initialize(), propagate(), optimize(). Then you built a model().
-# 3. Tuning the learning rate (which is an example of a "hyperparameter") can make a big difference to the algorithm. You will see more examples of this later in this course!
-
-# Finally, if you'd like, we invite you to try different things on this Notebook. Make sure you submit before trying anything. Once you submit, things you can play with include:
-#     - Play with the learning rate and the number of iterations
-#     - Try different initialization methods and compare the results
-#     - Test other preprocessings (center the data, or divide each row by its standard deviation)
-
-# Bibliography:
-# - http://www.wildml.com/2015/09/implementing-a-neural-network-from-scratch/
-# - https://stats.stackexchange.com/questions/211436/why-do-we-normalize-images-by-subtracting-the-datasets-image-mean-and-not-the-c
